28b0e8ea6ed46c1a365ede48cfa6df17b91ee681
VoidScientist <louthosi59@gmail.com>
2024-03-12 22:45:45 +0100

    initial commit

---
+++ local-server/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const {spawn} = require("child_process")
+
+const app = express();
+
+// SENDS THE HTML PAGE TO THE CLIENT
+function getPage(req, res) {
+
+    res.sendFile("C:/Users/baalp/OneDrive/Bureau/interface/index.html");
+
+}
+
+// CALLS THE PYTHON SCRIPT TO GET RESULTS AND SENDS THEM TO CLIENT
+function getHotelSorted(req, res) {
+
+    // ALLOWED ID PARAMS
+    const algos = ["bubblesort", "insertionsort", "selectionsort"];
+
+    // GET LIMIT FROM QUERY STRINGS
+    const limit = Number(req.query.limit) || 100;
+    
+    const algorithm = req.params.id;
+
+    // RETURN ERROR 404 IN CASE WRONG ID 
+    if (!algos.includes(algorithm)) {
+        res.json({status:"404", msg:"algorithm not found."});
+        return;
+    }
+
+    // CREATE THE PYTHON SUBPROCESS
+    const python = spawn("python", ["./test.py"]);
+
+    // GIVES ALGORITHM KEY TO SYS.STDIN.READ() IN PYTHON SCRIPT
+    python.stdin.end(algorithm);
+    
+    var hotels = [];
+
+    // RETRIEVE ALL OUTPUTS OF SCRIPT INTO HOTELS
+    python.stdout.on("data", data => {
+        for (let i of data.toString().split("\r\n")) {
+            if (i == 0) {continue;}
+            hotels.push(JSON.parse(i));
+        }
+    });
+
+    
+    // ONCE PYTHON PROCESS IS CLOSED, SEND DATA TO CLIENT
+    python.once("close", () => {
+        // LIMIT RESULTS ACC TO QUERY STRING LIMIT
+        hotels[0].result = hotels[0].result.slice(0,limit);
+        res.send({response: hotels[0]});
+});
+
+}
+
+// EXECUTES WHEN SERVER IS UP
+function setServerUp(){
+
+    console.log("Server listening on port 4001");
+
+}
+
+// WHAT FUNCTION TO CALL WHEN RECEIVING GET HTTP METHOD
+// AT SPECIFIC ENDPOINTS
+app.get("/", getPage);
+app.get("/search/:id", getHotelSorted);
+
+// STARTS THE SERVER
+app.listen(4001, setServerUp);
---
+++ local-server/test.py
@@ -0,0 +1,238 @@
+from time import perf_counter
+import sys
+import json
+
+def clean_up_hotel(hotel):
+    
+    rating = hotel["hotel_rating"]
+    experience = hotel["hotel_experience"].lower()
+
+    ratings = {
+        "excellent": 4.5,
+        "very good": 3.5,
+        "average": 2.5,
+        "poor": 1.5,
+        "terrible": 0.5,
+        "bad": 0
+    }
+
+    if not rating and not experience:
+        rating = 0
+        experience = "bad"
+
+    elif not rating and experience:
+        rating = ratings[experience]
+
+    elif not experience and rating:
+        experience = get_fitting_key(rating, ratings)
+
+    if not hotel["price"]:
+        hotel["price"] = 300
+
+    hotel["hotel_rating"] = rating
+    hotel["hotel_experience"] = experience
+
+    return hotel
+
+def get_fitting_key(value, dic):
+    
+    for k in dic:
+        
+        if float(value) < dic[k]: continue
+            
+        return k
+
+
+def get_hotel_info(keys, line):
+    hotel_infos = {}
+
+    string = False
+
+    elements = []
+    sub = ""
+
+    for i in line:
+
+        if i == "," and not string:
+            elements.append(sub)
+            sub = ""
+            continue
+
+        if i == '"':
+            string = not string
+            continue
+
+        if i == "\n": continue
+
+        sub += i
+
+    elements.append(sub)
+
+    hotel = dict(zip(keys, elements))
+
+    return clean_up_hotel(hotel)
+
+
+def parse_csv(filepath):
+    parsed = []
+
+    with open(filepath, "r") as f:
+        
+        keys = f.readline().replace("\n", "").split(",")
+
+        line = f.readline()
+
+        while line != "":
+            
+            parsed += [get_hotel_info(keys, line)]
+
+            line = f.readline()
+
+    return parsed
+
+
+def evaluate(hotel):
+    
+    if int(hotel["price"]) < 100:
+
+        p_score = 1.2
+        
+    elif int(hotel["price"]) < 150:
+        p_score = 1.1
+        
+    else:
+        p_score = 1
+
+    return float(hotel["hotel_rating"]) / 5 * len(hotel["amenities"]) * p_score
+
+
+def get_key_score_pairs(array, key=None):
+    
+    # establish [key, score] pairs, according to an optional key function.
+    
+    return [[i, key(v)] if key else [i, v] for i, v in enumerate(array)]
+
+
+def insertion_sort(array, key=None, reverse=False):
+    
+    pairs = get_key_score_pairs(array, key=key)
+
+    for i in range(1, len(pairs)):
+        
+        if pairs[i][1] < pairs[i - 1][1]:
+            
+            temp = pairs.pop(i)
+        
+            j = i - 1
+            
+            while True:
+                
+                if j < 0:
+                    
+                    pairs.insert(0, temp)
+                    
+                    break
+                
+                if temp[1] > pairs[j][1]:
+                
+                    pairs.insert(j + 1, temp)
+
+                    break
+                
+                j -= 1
+
+    result = [array[i[0]] for i in pairs]
+    
+    return result[::-1] if reverse else result
+
+
+# TODO: do the key pairs values at first iteration then cache.
+def bubble_sort(array, key=None, reverse=False):
+    
+    pairs = get_key_score_pairs(array, key=key)
+
+    i = 0
+    changed = False
+
+    # use of while because bubble sort doesn't guarantee the list to be sorted
+    # first time so we kinda don't know
+    while True:
+
+        # if there is no next value, restart at 0, unless there was no modifications last loop
+        # which means that the list is sorted.
+        if i + 1 == len(pairs):
+            
+            if not changed: break
+                
+            i = 0
+            
+            changed = False
+            
+            continue
+
+        # swaps [index, score] pairs if the score of pair i is larger than pair i + 1
+        if pairs[i][1] > pairs[i + 1][1]:
+            
+            pairs[i], pairs[i + 1] = pairs[i + 1], pairs[i]
+            
+            changed = True
+
+        i += 1
+
+    # from the [key, score] pairs, determine the sorted list.
+    result = [array[i[0]] for i in pairs]
+
+    return result[::-1] if reverse else result
+
+def selection_sort(array, key=None, reverse=False):
+    
+    pairs = get_key_score_pairs(array, key=key)
+    
+    for i in range(len(pairs)):
+        
+        min_ = [i, pairs[i][1]]
+        
+        for j in range(i, len(pairs)):
+              
+            min_ = [j, pairs[j][1]] if pairs[j][1] < min_[1] else min_
+                
+        pairs[i], pairs[min_[0]] = pairs[min_[0]], pairs[i]
+    
+    # from the [key, score] pairs, determine the sorted list.
+    result = [array[i[0]] for i in pairs]
+    
+    return result[::-1] if reverse else result
+
+def time_func(func, *args, **kwargs):
+    
+    start = perf_counter()
+
+    result = func(*args, **kwargs)
+
+    end = perf_counter()
+
+    #print(f"[{func.__name__}] executed in {end - start:.2f} seconds")
+
+    obj = {"name": func.__name__ ,"duration": f"{end-start:.2f}", "result": result}
+
+    return obj
+
+data = parse_csv("hotel_info_dedup.csv")
+
+func = {
+
+    "insertionsort": insertion_sort,
+    "bubblesort": bubble_sort,
+    "selectionsort": selection_sort
+
+}
+
+# CODE RELATED TO THE SERVER
+# THIS IS PROBABLY BAD
+# BUT I DIDN'T HAVE ANOTHER WAY LMAO
+
+funcname = sys.stdin.read()
+
+result = time_func(func[funcname], data, key=evaluate, reverse=True)
+
+print(json.dumps(result))
